e2c890c9dfade9e77ff280192b2fd07978eecc94
Harsh-2803 <rupharsh07@gmail.com>
2026-03-06 16:17:17 +0530

    UC9:Recursive Palindrome Check

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,62 +1,21 @@
 public class Palindrome {
 
-    static class Node {
-        char data;
-        Node next;
+    public static boolean isPalindrome(String str, int start, int end) {
 
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
+        if(start >= end)
+            return true;
 
-    public static boolean isPalindrome(String str) {
+        if(str.charAt(start) != str.charAt(end))
+            return false;
 
-        Node head = new Node(str.charAt(0));
-        Node current = head;
-
-        for(int i = 1; i < str.length(); i++) {
-            current.next = new Node(str.charAt(i));
-            current = current.next;
-        }
-
-        Node slow = head;
-        Node fast = head;
-
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node prev = null;
-        Node next;
-
-        while(slow != null) {
-            next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
-
-        Node first = head;
-        Node second = prev;
-
-        while(second != null) {
-            if(first.data != second.data)
-                return false;
-
-            first = first.next;
-            second = second.next;
-        }
-
-        return true;
+        return isPalindrome(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
 
         String str = "madam";
 
-        if(isPalindrome(str))
+        if(isPalindrome(str, 0, str.length() - 1))
             System.out.println("The string is a Palindrome");
         else
             System.out.println("The string is not a Palindrome");
