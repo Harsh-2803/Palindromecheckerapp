8bb9243dfb8ee8a906e03e595f3d44791865f3d3
Harsh-2803 <rupharsh07@gmail.com>
2026-03-06 16:27:41 +0530

    UC11:Object-Oriented Palindrome Service

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,29 +1,33 @@
+import java.util.Scanner;
+
 public class Palindrome {
 
-    public static void main(String[] args) {
+    public static boolean checkPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
 
-        String str = "A man a plan a canal Panama";
-
-        // Normalize string: remove spaces and convert to lowercase
-        str = str.replaceAll("\\s+", "").toLowerCase();
-
-        int start = 0;
-        int end = str.length() - 1;
-
-        boolean isPalindrome = true;
-
-        while(start < end) {
-            if(str.charAt(start) != str.charAt(end)) {
-                isPalindrome = false;
-                break;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
             }
-            start++;
-            end--;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (checkPalindrome(input)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
         }
 
-        if(isPalindrome)
-            System.out.println("The string is a Palindrome");
-        else
-            System.out.println("The string is not a Palindrome");
+        sc.close();
     }
 }
